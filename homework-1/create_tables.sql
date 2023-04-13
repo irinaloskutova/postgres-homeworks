795d7e5d064ac1ca5176764f1e211381adeb7b88
irinaloskutova <l.irinka@mail.ru>
2023-04-13 23:00:33 +0500

    дз1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	title text,
+	birth_date DATE,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id VARCHAR(100),
+	company_name VARCHAR(100),
+	contact_name VARCHAR(100)
+);
+
+CREATE TABLE orders
+(
+	order_id int,
+	customer_id VARCHAR(100),
+	employee_id int,
+	order_date DATE,
+	ship_city VARCHAR(50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,48 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import os
+import psycopg2
+
+customers = []
+employees = []
+orders = []
+
+with open('north_data/customers_data.csv', 'r') as f:
+    data = csv.DictReader(f)
+    for i in data:
+        customers.append(i)
+
+with open('north_data/employees_data.csv', 'r') as f:
+    data = csv.DictReader(f)
+    for i in data:
+        employees.append(i)
+
+with open('north_data/orders_data.csv', 'r') as f:
+    data = csv.DictReader(f)
+    for i in data:
+        orders.append(i)
+
+my_password = os.environ.get('PASSWORD_POSTGRESQL')
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password=my_password)
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            for i in range(len(customers)):
+                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', (customers[i]['customer_id'],
+                                                                          customers[i]['company_name'],
+                                                                          customers[i]['contact_name']))
+            for i in range(len(employees)):
+                cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s)', (employees[i]['first_name'],
+                                                                                  employees[i]['last_name'],
+                                                                                  employees[i]['title'],
+                                                                                  employees[i]['birth_date'],
+                                                                                  employees[i]['notes']))
+            for i in range(len(orders)):
+                cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (orders[i]['order_id'],
+                                                                               orders[i]['customer_id'],
+                                                                               orders[i]['employee_id'],
+                                                                               orders[i]['order_date'],
+                                                                               orders[i]['ship_city']))
+finally:
+    conn.close()
